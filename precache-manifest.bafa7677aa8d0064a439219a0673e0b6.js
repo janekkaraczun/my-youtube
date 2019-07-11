54a5d45c5642ebac486b02a32e78688cac694a2f
Jan Karaczun <janek.karaczun@yahoo.com>
2019-07-11 15:54:25 +0200

    Updates

---
+++ precache-manifest.bafa7677aa8d0064a439219a0673e0b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0ad59e68ec27d1511f66f762e59209",
+    "url": "/janekkaraczun/my-youtube/index.html"
+  },
+  {
+    "revision": "494bbc66de921b9a6022",
+    "url": "/janekkaraczun/my-youtube/static/js/2.f8f0dca0.chunk.js"
+  },
+  {
+    "revision": "9399bdd5a1a0c767fe1d",
+    "url": "/janekkaraczun/my-youtube/static/js/main.18de78dd.chunk.js"
+  },
+  {
+    "revision": "9eb496adc7696f70d104",
+    "url": "/janekkaraczun/my-youtube/static/js/runtime~main.d25c4bac.js"
+  }
+]);
